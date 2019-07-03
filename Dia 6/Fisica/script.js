246bbab3f6c2c5342a59d11e23b0a3ec3fefb78b
carolina-110 <carolina-110@hotmail.com>
2019-07-03 17:59:04 -0500

    Practica

--- Dia 6/Fisica/script.js
+++ Dia 6/Fisica/script.js
@@ -1,87 +1,57 @@
-let resultado = 0;
+
+
+
 let respuesta;
-let op1 = "";
-let op2 = "";
-let op3 = "";
 
 
-
-
-
-function obtener() {
-    respuesta = document.getElementById("respuesta");
-    console.log(respuesta);
-}
-
-
-// function deshabilitar() {
-    // op1 = document.getElementById("velocidad").value;
-    // op2 = document.getElementById("distancia").value;
-    // op3 = document.getElementById("tiempo").value;
-//         console.log(op1)
-//         console.log(op2)
-//         console.log(op3)
-
-//     if (op1 !== null && op2 !== null) {
-//         document.getElementById("tiempo").addEventListener("keypress", myFunction);
-//     } else if (op3 !== null && op2 !== null) {
-//         document.getElementById("velocidad").addEventListener("keypress", myFunction);
-//     } else if (op3 !== null && op1 !== null) {
-//         document.getElementById("distancia").addEventListener("keypress", myFunction);
-//     }
+// function obtener() {
+//     respuesta = document.getElementById("respuesta");
+//     console.log(respuesta);
 // }
+
+
+
 
 
 function deshabilitar() {
     op1 = document.getElementById("velocidad").value;
     op2 = document.getElementById("distancia").value;
     op3 = document.getElementById("tiempo").value;
-        console.log(op1)
-        console.log(op2)
-        console.log(op3)
 
-    if (op1 !== " " && op2 !== " ") {
-        document.getElementById("tiempo").addEventListener("keypress", myFunction);
-    } else if (op3 !== " " && op2 !== " ") {
-      //  document.getElementById("velocidad").addEventListener("keypress", myFunction);
-    //   op1.disabled=true;
-    } else if (op3 !== " " && op1 !== " ") {
-        document.getElementById("distancia").addEventListener("keypress", myFunction);
+
+    if (op1 !== "" && op2 !== "") {
+        document.getElementById("tiempo").disabled = true;
+    } else if (op3 !== "" && op2 !== "") {
+        document.getElementById("velocidad").disabled = true;
+    } else if (op3 !== "" && op1 !== "") {
+        document.getElementById("distancia").disabled = true;
+
     }
 }
 
 
-//  document.getElementById("tiempo").addEventListener("keypress", myFunction);
+
+function velocidadF() {
+
+    if (op1 === "") {
+        resultado = op2 / op3;
+        return resultado
+        //  alert(resultado);
+        // document.getElementById('respuesta').innerHTML = velocidadF()
 
 
-
-function myFunction() {
-    
-    document.getElementById("tiempo").disabled = true;
-    document.getElementById("velocidad").disabled = true;
-    document.getElementById("distancia").disabled = true;
-}
-
-
-
-
-
-
-
-function velocidad(velocidad, distancia, tiempo) {
-
-    if (velocidad === null) {
-        resultado = distancia / tiempo;
-        return resultado;
-    } else if (distancia === null) {
-        resultado = velocidad * tiempo
-        return resultado;
+    } else if (op2 === "") {
+        resultado = op1 * op3
+        // alert(resultado);
+        return resultado
     } else {
-        resultado = distancia / velocidad
-        return resultado;
+        resultado = op2 / op1
+        // alert(resultado);
+        return resultado
     }
 
 }
 
-
-console.log(velocidad(5, null, 2))
+function imprimirRespuesta() {
+    document.getElementById('respuesta').innerHTML = velocidadF();
+}
