898bbd72a97ee776bcb39cac6335d1089226fcf8
carolina-110 <carolina-110@hotmail.com>
2019-07-03 13:32:21 -0500

    Practica

--- Dia 6/Fisica/script.js
+++ Dia 6/Fisica/script.js
@@ -1,9 +1,9 @@
-
 let resultado = 0;
 let respuesta;
 let op1 = "";
 let op2 = "";
 let op3 = "";
+
 
 
 
@@ -14,21 +14,59 @@ function obtener() {
 }
 
 
-function deshabilitar(velocidad, distancia, tiempo) {
-    op1 = velocidad;
-    op2 = distancia;
-    op3 = tiempo
-    if (op1 !== null && op2 !== null) {
-        document.getElementById("tiempo").disabled = true;
-        
-    } else if (op3 && op2 !== "") {
-        document.getElementById("velocidad").disabled = true;
-    } else if (op3 && op1 !== "") {
-        document.getElementById("distancia").disabled = true;
-    }
-    
+// function deshabilitar() {
+    // op1 = document.getElementById("velocidad").value;
+    // op2 = document.getElementById("distancia").value;
+    // op3 = document.getElementById("tiempo").value;
+//         console.log(op1)
+//         console.log(op2)
+//         console.log(op3)
 
+//     if (op1 !== null && op2 !== null) {
+//         document.getElementById("tiempo").addEventListener("keypress", myFunction);
+//     } else if (op3 !== null && op2 !== null) {
+//         document.getElementById("velocidad").addEventListener("keypress", myFunction);
+//     } else if (op3 !== null && op1 !== null) {
+//         document.getElementById("distancia").addEventListener("keypress", myFunction);
+//     }
+// }
+
+
+function deshabilitar() {
+    op1 = document.getElementById("velocidad").value;
+    op2 = document.getElementById("distancia").value;
+    op3 = document.getElementById("tiempo").value;
+        console.log(op1)
+        console.log(op2)
+        console.log(op3)
+
+    if (op1 !== " " && op2 !== " ") {
+        document.getElementById("tiempo").addEventListener("keypress", myFunction);
+    } else if (op3 !== " " && op2 !== " ") {
+      //  document.getElementById("velocidad").addEventListener("keypress", myFunction);
+    //   op1.disabled=true;
+    } else if (op3 !== " " && op1 !== " ") {
+        document.getElementById("distancia").addEventListener("keypress", myFunction);
+    }
 }
+
+
+//  document.getElementById("tiempo").addEventListener("keypress", myFunction);
+
+
+
+function myFunction() {
+    
+    document.getElementById("tiempo").disabled = true;
+    document.getElementById("velocidad").disabled = true;
+    document.getElementById("distancia").disabled = true;
+}
+
+
+
+
+
+
 
 function velocidad(velocidad, distancia, tiempo) {
 
